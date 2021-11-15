5109ba925023d8e4eeeee539ea65ee34f1a31bca
Vftdan <vftdan@gmail.com>
2021-11-15 21:13:28 +0300

    Practice 8: use simple (primary key based) ModelSerializer instead of HyperlinkedModelSerializer
    We do not want to expose all dependent entities via api

--- Practice 8/Zakirullin/librarysystem/mainapp/serializers.py
+++ Practice 8/Zakirullin/librarysystem/mainapp/serializers.py
@@ -2,44 +2,44 @@ from rest_framework import serializers
 from .models import Reader, Book, Publisher, Category, Copy, Borrowing, BookCat
 
 
-class ReaderSerializer(serializers.HyperlinkedModelSerializer):
+class ReaderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reader
         fields = ['number', 'last_name', 'first_name', 'address', 'birth_date']
 
 
-class BookSerializer(serializers.HyperlinkedModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['isbn', 'title', 'author', 'pages_num', 'pub_year',
                   'publisher']
 
 
-class PublisherSerializer(serializers.HyperlinkedModelSerializer):
+class PublisherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Publisher
         fields = ['pub_name', 'pub_address']
 
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['category_name', 'parent_cat']
 
 
-class CopySerializer(serializers.HyperlinkedModelSerializer):
+class CopySerializer(serializers.ModelSerializer):
     class Meta:
         model = Copy
         fields = ['book', 'copy_number', 'shelf_position']
 
 
-class BorrowingSerializer(serializers.HyperlinkedModelSerializer):
+class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = ['book_copy', 'shelf_position']
 
 
-class BookCatSerializer(serializers.HyperlinkedModelSerializer):
+class BookCatSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookCat
         fields = ['book', 'category']
